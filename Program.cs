16ff87eb6021a277a65d22f2e719083713ea0f33
Dhruv Iyer <63696398+dhruv1711@users.noreply.github.com>
2022-05-06 17:23:06 +0900

    Base files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AIWolf.Client;
+using AIWolf.Lib;
+
+namespace MyAgent
+{
+    class MyAgentApp
+    {
+        static void Main(string[] args)
+        {
+            new ClientStarter(new DummyPlayer(), args).Start();
+        }
+    }
+}
